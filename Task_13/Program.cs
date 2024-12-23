355ebf08f0a1a5a4f5af60385979d5a14ab6838d
Dimitry-P <dimitry.piatnitsky@yahoo.com>
2024-12-23 16:55:32 +0300

    Add project files.

---
+++ Task_13/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_13
+{
+    internal class Program
+    {
+        //•	Задача 13: Напишите программу, которая делит число на два и выводит результат.
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число:");
+            string number = Console.ReadLine();
+            float result = float.Parse(number);
+            Console.WriteLine("Результат: "+result/2);
+        }
+    }
+}
